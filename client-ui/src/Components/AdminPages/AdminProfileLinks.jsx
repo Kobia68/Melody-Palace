67379ef6462c501e0d0e5f372649ab3c281a1966
Newton Kobia <newtonmutethia100@gmail.com>
2024-08-23 18:22:25 +0300

    A minor change on the Links in the AdminProfileLinks component

--- client-ui/src/Components/AdminPages/AdminProfileLinks.jsx
+++ client-ui/src/Components/AdminPages/AdminProfileLinks.jsx
@@ -6,7 +6,7 @@ export const AdminProfileLinks = () => {
     <div className="flex justify-around pt-5 w-1/2 mx-auto mt-20">
       <Link
         className="bg-primary text-white rounded-full px-4 py-2"
-        to="newevent"
+        to=""
       >
         New Event
       </Link>
