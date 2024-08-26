7b95eacbd7da9f116e62d5ec95cce6ad170dfeb3
Newton Kobia <newtonmutethia100@gmail.com>
2024-08-26 18:43:09 +0300

    Added the footer and configured its routes

--- client-ui/src/Components/Layout.jsx
+++ client-ui/src/Components/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Navbar } from './Navbar/Navbar'
 import { Outlet } from 'react-router-dom';
+import { Footer } from './Footer';
 
 export const Layout = () => {
   return (
     <div className='p-4 flex flex-col min-h-screen'>
         <Navbar />
         <Outlet />
+        <Footer />
     </div>
   );
 }
