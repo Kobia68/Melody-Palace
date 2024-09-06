b8f58a4abb7c2799482ae0725f9333eb6af1a6ef
Newton Kobia <newtonmutethia100@gmail.com>
2024-09-06 19:38:32 +0300

    added the code to check if a new usr registering is already in the database or not

--- API/controllers/authController.js
+++ API/controllers/authController.js
@@ -1,4 +1,14 @@
+import { db } from "../helpers/db";
+
 export const signUp = (req, res) =>{
+
+    // CHECK IF DATA ALREADY EXISTS
+    const q = "SELECT * FROM users WHERE email = ? or username = ?";
+
+    db.query(q, [req.body.email, req.body.username], (err, data) => {
+        if(err) return res.json(err)
+        if(data.length) return res.status(409).json("User already exists")
+    })
     
 }
 export const signIn = (req, res) =>{
