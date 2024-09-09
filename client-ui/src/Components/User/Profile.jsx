bbd0cd4aeb5db87dfdcbcd29ffe209a391c3fb17
Newton Kobia <newtonmutethia100@gmail.com>
2024-09-10 00:14:11 +0300

    Modified the profile page to display the details of the user who is currently signed in

--- client-ui/src/Components/User/Profile.jsx
+++ client-ui/src/Components/User/Profile.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { AuthContext } from '../../context/authContext';
 
 export const Profile = () => {
+  const { currentUser } = useContext(AuthContext);
+
   return (
-    <div className='w-2/3 mt-5 mx-auto flex flex-row'>
-      <div className='w-full mt-10'>
-        <div className='flex mt-10 w-2/3 mx-auto gap-4'>
-          <label htmlFor="" className='font-semibold w-32'>Name:</label>
-          <p className='border-b border-gray-500 w-3/4 pl-5 text-lg'>John Doe</p>
+    <div className="w-2/3 mt-5 mx-auto flex flex-row">
+      <div className="w-full mt-10">
+        <div className="flex mt-10 w-2/3 mx-auto gap-4">
+          <label htmlFor="" className="font-semibold w-32">
+            Name:
+          </label>
+          <p className="border-b border-gray-500 w-3/4 pl-5 text-md">
+            {currentUser?.fullname}
+          </p>
         </div>
-        <div className='flex mt-10 w-2/3 mx-auto gap-4'>
-          <label htmlFor="" className='font-semibold w-32'>Email:</label>
-          <p className='border-b border-gray-500 w-3/4 pl-5 text-lg'>johndoe@email.com</p>
+        <div className="flex mt-10 w-2/3 mx-auto gap-4">
+          <label htmlFor="" className="font-semibold w-32">
+            Email:
+          </label>
+          <p className="border-b border-gray-500 w-3/4 pl-5 text-md">
+            {currentUser?.email}
+          </p>
         </div>
-        <div className='flex mt-10 w-2/3 mx-auto gap-4'>
-          <label htmlFor="" className='font-semibold w-32'>username</label>
-          <p className='border-b border-gray-500 w-3/4 pl-5 text-lg'>johndoe</p>
+        <div className="flex mt-10 w-2/3 mx-auto gap-4">
+          <label htmlFor="" className="font-semibold w-32">
+            username
+          </label>
+          <p className="border-b border-gray-500 w-3/4 pl-5 text-md">
+            {currentUser?.username}
+          </p>
         </div>
-        <div className='flex mt-10 w-2/3 mx-auto gap-4'>
-          <label htmlFor="" className='font-semibold w-32'>Name:</label>
-          <p className='border-b border-gray-500 w-3/4 pl-5 text-lg'>071864827874</p>
-        </div>
-        <div className='flex mt-10 w-2/3 mx-auto gap-4'>
-          <label htmlFor="" className='font-semibold w-32'>Password:</label>
-          <p className='border-b border-gray-500 w-3/4 pl-5 text-lg flex justify-between'>*********
-          <Link className='bg-primary text-white rounded-full px-2 mb-1'>Change Password</Link></p>
+        <div className="flex mt-10 w-2/3 mx-auto gap-4">
+          <label htmlFor="" className="font-semibold w-32">
+            PhoneNo:
+          </label>
+          <p className="border-b border-gray-500 w-3/4 pl-5 text-md">
+            {currentUser?.phoneNo}
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
